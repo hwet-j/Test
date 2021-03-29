73249f4633b6428e4b9d8d954cd5508ebac56afd
hwet-j <ghlckd5424@gmail.com>
2021-03-29 16:46:56 +0900

    반복문 while

--- pro1/pack1/test5while.py
+++ pro1/pack1/test5while.py
@@ -9,6 +9,7 @@ print('\na : ', a)
 print()
 
 i = 1
+# while문 안에 while문 
 while i <= 3:
     j = 1
     while j <= 4:
@@ -20,6 +21,7 @@ while i <= 3:
 print('계속')
 print('1 ~ 100 사이의 3의 배수의 합')
 i = 1; hap = 0
+# while문 안에 if문
 while i <= 100:
     if i % 3 == 0:
         hap += i
@@ -68,15 +70,15 @@ while i <= 10:
 # if 블럭 내에 while 블럭
 import time
 sw = input('폭탄 스위치를 누를까요?[y/n]')
-if sw == 'Y' or sw == 'y':
+if sw == 'Y' or sw == 'y':  # 기본적으로 대소문자 구분함(그렇기 때문에 조건에 둘 다 작성해줘야함)
     count = 5
     while 1 <= count:
         print('%d 초 남았어요' %count)
-        time.sleep(1)
+        time.sleep(1)   # 딜레이 1초
         count -= 1
     print('폭발') 
 elif sw == 'N' or sw == 'n':
     print('작업 취소')
 else:
-    print('y 또는 n을 누르시오')
+    print('y 또는 n이 눌리지 않았습니다.')
 
